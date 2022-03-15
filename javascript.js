69fc360b53a9d7e48cbf7bd641c227fe50f1de53
jiffylube <lu.jeffrey91@gmail.com>
2022-03-15 16:07:26 -0400

    button press jump created

--- javascript.js
+++ javascript.js
@@ -1,13 +1,5 @@
-//add foreground
-//add background
-
-//define playArea
 //define sprite location, make x and y axis a constant?
-//will need to move this to default location after restart.
 
-//default Y axis decrease for object
-
-//create function that will increase Y axis for sprite (move up)
 //add event listener for 'spacebar'
 
 //add X axis gradual slide
@@ -19,11 +11,25 @@ let playArea = document.querySelector('.playArea')
 let ground = document.querySelector('.ground')
 let sky = document.querySelector('.sky')
 
+
+
+//position bird
 let birdLeft = 220;
 let birdBottom = 150;
+let gravity = 3;
 
+//startGame , bird will fall, with interval set
 let startGame = function () {
+  birdBottom -= gravity;
   bird.style.bottom = birdBottom + 'px';
+  bird.style.left = birdLeft + 'px';
+}
+// let timeID = setInterval(startGame, 20);
+
+//jump
+let jump = function (e) {
+  birdBottom += 50;
+  bird.style.bottom = birdBottom + 'px'
 }
 
-startGame();
+document.addEventListener('keypress', jump)
