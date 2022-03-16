7a60aaf9d13835678db8f29c23302e5fa0c9bf45
jiffylube <lu.jeffrey91@gmail.com>
2022-03-16 15:25:57 -0400

    poles are moving!

--- javascript.js
+++ javascript.js
@@ -1,7 +1,3 @@
-//define sprite location, make x and y axis a constant?
-
-//add event listener for 'spacebar'
-
 //add X axis gradual slide
 //add clear zone , add 1 point!
 //else 'GG' and restart on tap
@@ -18,7 +14,6 @@ let birdLeft = 220;
 let birdBottom = 150;
 let gravity = 2;
 
-
 //startGame , bird will fall, with interval set
 let startGame = function () {
   birdBottom -= gravity;
@@ -26,7 +21,6 @@ let startGame = function () {
   bird.style.left = birdLeft + 'px';
 }
 
-// let timeID = setInterval(startGame, 20);
 
 //jump function + spacebar jump only
 let jump = function () {
@@ -45,3 +39,29 @@ let spaceBar = function (e) {
 }
 
 document.addEventListener('keypress', spaceBar)
+
+
+//add crash zones
+
+let makePoles = function () {
+  let obstacleLeft = 500
+  let randomHeight = Math.random() * 200
+  let obstacleBottom = randomHeight
+  const obstacle = document.createElement('div')
+  obstacle.classList.add('pole')
+  playArea.appendChild(obstacle);
+  obstacle.style.left = obstacleLeft + 'px'
+  obstacle.style.bottom = obstacleBottom + 'px'
+  obstacle.style.top = randomHeight
+  
+  // move the created poles   
+  let movePoles = function () {
+    obstacleLeft -= 2;
+    obstacle.style.left = obstacleLeft + 'px'
+  }
+  
+  setInterval(movePoles , 20)
+}
+
+// let timeID = setInterval(startGame, 20);
+// setInterval(makePoles , 1400)
